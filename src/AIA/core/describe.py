4e1a4d2830c0923589af78886d6934577a8c4311
yexner <65974612+yexner@users.noreply.github.com>
2025-03-06 16:58:41 +0100

    Added template files and functions for new implementation of features

---
+++ src/AIA/core/describe.py
@@ -0,0 +1,56 @@
+"""
+Add general comments here.
+"""
+
+# Imports
+from tqdm import tqdm
+import numpy as np
+
+def describe_blip(self, df_images): # TODO: Implement this function
+    """
+    Describe what the function does here.
+
+    :param self: AIA object
+    :param df_images: DataFrame containing a 'filename' column with paths to image files
+    :return: DataFrame with added feature columns
+    """
+
+    # Create a copy of the input DataFrame to store results
+    df = df_images.copy()
+
+    # Initialize new columns with NaN
+    df['featureName'] = np.nan
+
+    # Iterate over all image paths
+    for idx, image_path in enumerate(tqdm(df_images['filename'])):
+        # Implement feature computation here.
+
+        # Store computed features in DataFrame
+        df.loc[idx, 'featureName'] = feature_value
+
+    return df
+
+def describe_llm(self, df_images): # TODO: Implement this function
+    """
+    Describe what the function does here.
+
+    :param self: AIA object
+    :param df_images: DataFrame containing a 'filename' column with paths to image files
+    :return: DataFrame with added feature columns
+    """
+
+    # Create a copy of the input DataFrame to store results
+    df = df_images.copy()
+
+    # Initialize new columns with NaN
+    df['featureName'] = np.nan
+
+    # Iterate over all image paths
+    for idx, image_path in enumerate(tqdm(df_images['filename'])):
+        # Implement feature computation here.
+
+        # Store computed features in DataFrame
+        df.loc[idx, 'featureName'] = feature_value
+
+    return df
+
--- src/AIA/core/visual_complexity.py
+++ src/AIA/core/visual_complexity.py
@@ -30,3 +30,27 @@ def visual_complexity(self, df_images):
         df.loc[idx, 'visualComplexity'] = r_spt
         
     return df
+
+def self_similarity(self, df_images):
+    """
+    Describe what the function does here.
+
+    :param self: AIA object
+    :param df_images: DataFrame containing a 'filename' column with paths to image files
+    :return: DataFrame with added feature columns
+    """
+
+    # Create a copy of the input DataFrame to store results
+    df = df_images.copy()
+
+    # Initialize new columns with NaN
+    df['featureName'] = np.nan
+
+    # Iterate over all image paths
+    for idx, image_path in enumerate(tqdm(df_images['filename'])):
+        # Implement feature computation here.
+
+        # Store computed features in DataFrame
+        df.loc[idx, 'featureName'] = feature_value
+
+    return df
